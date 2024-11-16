4dce87083de60658529f2c574aa441443b9ef17b
Santeri <santeri.hyvarinen12@gmail.com>
2024-11-17 00:44:23 +0200

    Add collision system

---
+++ valoa elias tapani kansalle/collision/CollisionSystem.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using valoa_elias_tapani_kansalle.entities;
+
+
+namespace valoa_elias_tapani_kansalle.collision
+{
+    public class CollisionSystem 
+    {
+        public static GameObject IsColliding(GameObject obj, List<GameObject> objects)
+        {
+            foreach(var otherObj in objects)
+            {
+                if( otherObj != obj && obj.BoundingBox.Intersects(otherObj.BoundingBox)) 
+                {
+                    return otherObj;
+                }
+            }
+            return null;
+        }
+    }
+}
+
+/*
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+
+        }
+
+    }
+}
+*/
