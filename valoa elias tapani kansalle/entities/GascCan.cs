126eb6f9c83b3c915e5c8be41a2709b94af0cb33
VerneriPudas <verkku.pudas@gmail.com>
2024-11-17 11:32:19 +0200

    adds gascan and generator

---
+++ valoa elias tapani kansalle/entities/GascCan.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace valoa_elias_tapani_kansalle.entities
+{
+    public class GasCan : Interactable
+    {
+        private bool attachToPlayer;
+        private bool yeeted;
+        private Texture2D gascan;
+        public Texture2D _Gascan
+        {
+            get { return gascan; }
+            set { gascan = value; }
+        }
+        public GasCan(Vector2 pos)
+        {
+            Position = pos;
+            attachToPlayer = false;
+            interactable = true;
+            yeeted = false;
+        }
+        public override void LoadContent(ContentManager content)
+        {
+            gascan = content.Load<Texture2D>("sprites/gascan");
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (!attachToPlayer && !yeeted)
+            {
+                spriteBatch.Draw(gascan,
+                                 Position,
+                                 null,
+                                 Color.White,
+                                 0,
+                                 new Vector2(16, 16),
+                                 new Vector2(2, 2),
+                                 SpriteEffects.None,
+                                 EntityUtil.GetEntityLayer(EntityLayer.ENTITY_LAYER_INTERACTABLE));
+            }
+        }
+
+        public override void Update(GameTime gameTime, Player player)
+        {
+            base.Update(gameTime);
+            if (Input.IsKeyPressed(Keys.F))
+            {
+                if (canInteract(player) && player.itemHeld == null)
+                {
+                    attachToPlayer = true;
+                    player.itemHeld = "gascan";
+                }
+            }
+
+            if (attachToPlayer)
+            {
+                if (player.itemHeld == null)
+                {
+                    Position = new Vector2(-9999, -9999);
+                    attachToPlayer = false;
+                    yeeted = true;
+                }
+                Position = player.Position;
+                System.Diagnostics.Debug.WriteLine($"Item Position Updated: {Position}");
+                if (Input.IsKeyPressed(Keys.F))
+                {
+                    System.Diagnostics.Debug.WriteLine("Attached");
+                }
+                if (Input.IsKeyPressed(Keys.G))
+                {
+                    System.Diagnostics.Debug.WriteLine("Dropping");
+                    attachToPlayer = false;
+                    player.itemHeld = null;
+
+                }
+            }
+        }
+    }
+}
---
+++ valoa elias tapani kansalle/entities/Generator.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace valoa_elias_tapani_kansalle.entities
+{
+    public class Generator : Interactable
+    {
+        private bool fueled;
+        private bool _fixed;
+        private Texture2D _generator;
+        public Texture2D _Generator
+        {
+            get { return _generator; }
+            set { _generator = value; }
+        }
+
+
+        public Texture2D DebugTexture { get; set; }
+        public Generator(Vector2 pos)
+        {
+            Position = pos;
+            interactable = true;
+            IsCollisionActive = true;
+        }
+        public override void LoadContent(ContentManager content)
+        {
+            _generator = content.Load<Texture2D>("sprites/generator");
+            BoundingBox = new Rectangle((int)Position.X, (int)Position.Y, _generator.Width, _generator.Height);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(_generator,
+                                Position,
+                                null,
+                                Color.White,
+                                0,
+                                Vector2.Zero,
+                                Vector2.One,
+                                SpriteEffects.None,
+                                EntityUtil.GetEntityLayer(EntityLayer.ENTITY_LAYER_INTERACTABLE));
+
+            spriteBatch.Draw(DebugTexture, BoundingBox, Color.White);
+        }
+
+        public override void Update(GameTime gameTime, Player player)
+        {
+            base.Update(gameTime);
+            if (Input.IsKeyPressed(Keys.F) && canInteract(player))
+            {
+                if (player.itemHeld == "gasCan")
+                {
+                    player.itemHeld = null;
+                    fueled = true;
+                } else if (player.itemHeld == "screwdriver")
+                {
+                    player.itemHeld = null;
+                    _fixed = true;
+                }
+            }
+            if (_fixed && fueled)
+            {
+                // Win condition
+                System.Diagnostics.Debug.WriteLine("Voitit pelin!");
+            }
+        }
+    }
+}
